7c1bda9a3b52bfa905a5c27fd9768d1c268dba94
Rong Bao <rong.bao@csmantle.top>
2026-07-04 14:35:27 +0800

    Use tabs in shell scripts

--- scripts/generate-index.sh
+++ scripts/generate-index.sh
@@ -7,13 +7,13 @@ DATE_PLACEHOLDER='@@DATE@@'
 
 entries=""
 for f in $(find . -maxdepth 1 -name '*.pdf' -type f | sort); do
-        name="$(basename "$f")"
-        entry="$(printf '<li><a href="%s" type="application/pdf">%s</a></li>' "$name" "$name")"
-        entries="$entries$entry"
+	name="$(basename "$f")"
+	entry="$(printf '<li><a href="%s" type="application/pdf">%s</a></li>' "$name" "$name")"
+	entries="$entries$entry"
 done
 
 date="$(date --iso-8601 seconds --universal)"
 sed \
-        -e "s|$PDF_ENTRIES_PLACEHOLDER|$entries|g" \
-        -e "s|$DATE_PLACEHOLDER|$date|g" \
-        "$1" >"$2"
+	-e "s|$PDF_ENTRIES_PLACEHOLDER|$entries|g" \
+	-e "s|$DATE_PLACEHOLDER|$date|g" \
+	"$1" >"$2"
